47c0c544c1195a5b333c07374a646d180468873d
prodoxx <reggie.escobar94@gmail.com>
2021-11-30 03:15:42 -0600

    Updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,12 @@ module.exports = {
         {
             name: 'web-app',
             exec_mode: 'cluster',
-            instances: 'max', // Or a number of instances
+            instances: 1, // Or a number of instances
             script: './node_modules/nuxt/bin/nuxt.js',
             args: 'start',
+            env: {
+                PORT: 8080,
+            },
         },
     ],
 };
