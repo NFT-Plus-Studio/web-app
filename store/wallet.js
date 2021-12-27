c1ac29c2a291376f25cdba11c40cfaacb0a5c8f7
prodoxx <reggie.escobar94@gmail.com>
2021-12-27 12:03:22 -0600

    Added a wallet store.

--- store/wallet.js
+++ store/wallet.js
@@ -1,9 +1,17 @@
 export const state = () => ({
+    connected: false,
     address: null,
+    providerChainId: null,
 });
 
 export const mutations = {
     setAddress(state, address) {
         state.address = address;
+    },
+    setProviderChainId(state, chainId) {
+        state.providerChainId = chainId;
+    },
+    setConnected(state, connected) {
+        state.connected = connected;
     },
 };
